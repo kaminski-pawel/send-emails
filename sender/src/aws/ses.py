9d08bd103d2409a9e0b8956a6f8519c6713a7f7b
Pawel Kaminski <pawelkaminski@tipi.software>
2021-11-26 23:03:47 +0100

    Add ses service error handling

--- sender/src/aws/ses.py
+++ sender/src/aws/ses.py
@@ -1,6 +1,16 @@
-def send_raw_email(client, sender, recipients, raw_message, config=''):
-    client.send_raw_email(
-            Source=sender,
-            Destinations=recipients,
-            RawMessage={'Data': raw_message},
-            ConfigurationSetName=config)
+from botocore.exceptions import ClientError
+
+
+def send_raw_email(client, sender, recipients, raw_message):
+    try:
+        response = client.send_raw_email(
+                Source=sender,
+                Destinations=recipients,
+                RawMessage={'Data': raw_message})
+    except ClientError as e:
+        _log(e.response['Error']['Message'])
+    else:
+        _log(f'Email sent. Message ID: {response["MessageId"]}'),
+
+def _log(msg):
+    print(msg)
