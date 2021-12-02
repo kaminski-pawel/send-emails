2273e2ba153dc65a56a46ae7adf212d630f424b1
Pawel Kaminski <pawelkaminski@tipi.software>
2021-12-02 18:38:22 +0100

    Add handling no file exceptions

--- sender/src/utils.py
+++ sender/src/utils.py
@@ -1,18 +1,33 @@
 import csv
 import json
+import os
 
 
 def open_json_file(filename):
+    return _error_handler(filename, _open_json_file)
+
+def open_html_file(filename):
+    return _error_handler(filename, _open_html_file)
+
+def open_csv_file(filename):
+    return _error_handler(filename, _open_csv_file)
+
+def _open_json_file(filename):
     with open(filename, 'r') as file:
         data = json.load(file)
     return data
 
-def open_html_file(filename):
+def _open_html_file(filename):
     with open(filename, 'r') as file:
         html = file.read()
     return html
 
-def open_csv_file(filename):
+def _open_csv_file(filename):
     with open(filename, newline='') as file:
         values = [row[0] for row in csv.reader(file, delimiter=';')]
     return values
+
+def _error_handler(filename, funct):
+    if os.path.isfile(filename):
+        return funct(filename)
+    raise FileNotFoundError(f'File {filename} was not found.')
